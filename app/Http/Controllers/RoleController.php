5ac9ada69abcd14f55eda578def58d83e56fcd58
Aftab Tunio <aftabatunio@gmail.com>
2022-10-05 15:44:30 +0500

    commit

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use DB;
+
+class RoleController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    
+    public function index(Request $request)
+    {
+        $data = Role::orderBy('id','DESC')->paginate(5);
+        return view('roles.index',compact('data'))
+            ->with('i', ($request->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Role $role)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Role $role)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Role $role)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Role $role)
+    {
+        //
+    }
+}
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,13 @@
+
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <table cellpadding="10">
+            <tr>
+                <td><a href="home">Home</a></td>
+                <td><a href="users">Users</a></td>
+                <td><a href="roles">Roles</a></td>
+                <td><a href="products">Products</a></td>
+            </tr>
+        </table>
+    </div>
+</div>
